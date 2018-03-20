8159b28ccaaef0c17769b91a34ea9d13a85a1edb
Barış Teksin <b.teksin.bt@gmail.com>
2018-03-20 18:03:25 +0300

    Bölmesiz Bölme Upload

--- bölmesiz_bölme.py
+++ bölmesiz_bölme.py
@@ -1,24 +1,13 @@
-def ilk(sayi,bölünecek,sayac):
-    sayac+=1
-    if sayac*bölünecek==sayi:
-        return sayac
-    elif sayac*bölünecek>sayi:
-        return ondalıklı(sayi,bölünecek,sayac)
-    else:
-        return ilk(sayi,bölünecek,sayac)
-def ondalıklı(sayi,bölünecek,sayac):
-    sayac+=0.1
-    if sayac%1==0.9:
-        if sayac*bölünecek==sayi:
-            return sayac
-        else:
-            return sayac
-    else:
-        if sayac*bölünecek==sayi:
-            return sayac
-        else:
-            return ondalıklı(sayi,bölünecek,sayac)
+def bol(bolunen, bolen):
+  if bolunen == 0:
+    return 0
+  if bolen <= bolunen:
+    print(bolunen,bolen)
+    return 1 + bol(bolunen-bolen, bolen)
+  else:
+    print(bolunen, bolen)
+    return 0.1 * (1+bol(bolunen*10-bolen, bolen))
 sayi=float(input("Bir sayi girin"))
 bölünecek=float(input("Kaça bölünecek"))
 sayac=0.0
-print(ilk(sayi,bölünecek,sayac,sayac2))
+print(bol(sayi,bölünecek))
